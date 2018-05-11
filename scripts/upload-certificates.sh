d303cf8c91a772d09ccd3eab5eaa0f30e4bdf0e9
Reme Hetman <remek@renek.net>
2018-05-11 14:22:06 -0400

    update
    update

--- scripts/upload-certificates.sh
+++ scripts/upload-certificates.sh
@@ -2,7 +2,7 @@ kmskey=$1
 region=$2
 aws s3 cp s3://tls-blog-cf/wildcard-ec2-internal.crt wildcard-ec2-internal.crt
 aws s3 cp s3://tls-blog-cf/wildcard-ec2-internal.key wildcard-ec2-internal.key
-aws ssm put-parameter --name /emr/certificate --value file://wildcard-ec2-internal.crt --type SecureString --key-id $kmskey --overwrite --region $region
-aws ssm put-parameter --name /emr/inter-nodes-certificate --value file://wildcard-ec2-internal.crt --type SecureString --key-id $kmskey --overwrite --region $region
-aws ssm put-parameter --name /emr/private-key --value file://wildcard-ec2-internal.key --type SecureString --key-id $kmskey --overwrite --region $region
-aws ssm put-parameter --name /emr/inter-nodes-private-key --value file://wildcard-ec2-internal.key --type SecureString --key-id $kmskey --overwrite --region $region
+aws ssm put-parameter --name /emr/certificate --value fileb://wildcard-ec2-internal.crt --type SecureString --key-id $kmskey --overwrite --region $region
+aws ssm put-parameter --name /emr/inter-nodes-certificate --value fileb://wildcard-ec2-internal.crt --type SecureString --key-id $kmskey --overwrite --region $region
+aws ssm put-parameter --name /emr/private-key --value fileb://wildcard-ec2-internal.key --type SecureString --key-id $kmskey --overwrite --region $region
+aws ssm put-parameter --name /emr/inter-nodes-private-key --value fileb://wildcard-ec2-internal.key --type SecureString --key-id $kmskey --overwrite --region $region

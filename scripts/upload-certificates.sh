842f11b7e846289eb16f5700f515ff201dd2562f
Reme Hetman <remek@renek.net>
2018-06-14 15:44:26 -0400

    Update
    Update

--- scripts/upload-certificates.sh
+++ scripts/upload-certificates.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 kmskey=$1
 region=$2
-aws s3 cp s3://tls-blog-cf/wildcard-ec2-internal.crt wildcard-ec2-internal.crt
-aws s3 cp s3://tls-blog-cf/wildcard-ec2-internal.key wildcard-ec2-internal.key
+aws s3 cp s3://tls-blog-cf/wildcard-ec2-internal.crt ./wildcard-ec2-internal.crt
+aws s3 cp s3://tls-blog-cf/wildcard-ec2-internal.key ./wildcard-ec2-internal.key
 aws ssm put-parameter --name /emr/certificate --value fileb://wildcard-ec2-internal.crt --type SecureString --key-id $kmskey --overwrite --region $region
 aws ssm put-parameter --name /emr/inter-nodes-certificate --value fileb://wildcard-ec2-internal.crt --type SecureString --key-id $kmskey --overwrite --region $region
 aws ssm put-parameter --name /emr/private-key --value fileb://wildcard-ec2-internal.key --type SecureString --key-id $kmskey --overwrite --region $region
 aws ssm put-parameter --name /emr/inter-nodes-private-key --value fileb://wildcard-ec2-internal.key --type SecureString --key-id $kmskey --overwrite --region $region
+
